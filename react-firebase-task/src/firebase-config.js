12ddbd026cfaa9e675c6baa091f4dea29342ba8b
Vikas Jyani <vickbishnoi@gmail.com>
2022-02-02 14:39:23 +0530

    added firebase to the project

---
+++ react-firebase-task/src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVADdVb_2MRuKsGwluoyfZZ2nYttPYMb8",
+  authDomain: "react-firebase-task-8b3fe.firebaseapp.com",
+  projectId: "react-firebase-task-8b3fe",
+  storageBucket: "react-firebase-task-8b3fe.appspot.com",
+  messagingSenderId: "1031389152331",
+  appId: "1:1031389152331:web:ecb6ddda3f5505092939ce",
+  measurementId: "G-LKQEP6TQGP",
+};
+
+const app = initializeApp(firebaseConfig);
